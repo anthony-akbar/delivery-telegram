2e7248d627b7c79cf906c4b57df96d580d48a1ec
anthony-akbar <mamadjanovakbar@gmail.com>
2023-02-22 11:49:37 +0500

    text color test

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         console.log('Axios started...')
         var style = getComputedStyle(document.body)
         this.setState({color: style.getPropertyValue('--tg-theme-button-text-color')})
+        this.setState({bg: style.getPropertyValue('--tg-theme-bg-color')})
         axios.get("https://delivery.royale.uz/api/products").then((res) => {
             this.setState({data: res.data.data})
             console.log(res.data.data)
@@ -25,7 +26,7 @@ class App extends Component {
         return (
             <div>
                 <div>
-                    {this.state.color}
+                    {this.state.color} |||| {this.state.bg}
                 </div>
                 <Products data={this.state.data}/>
             </div>
