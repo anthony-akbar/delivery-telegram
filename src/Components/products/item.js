52637ccc3619b447234519a585fb666267142848
anthony-akbar <mamadjanovakbar@gmail.com>
2023-02-22 11:22:32 +0500

    Telegram integration

--- src/Components/products/item.js
+++ src/Components/products/item.js
@@ -6,8 +6,11 @@ class ProductItem extends Component {
     render() {
         const {title, image, receipt, status, id} = this.props
         return (
-            <Card className={'col-4'}>
-                <Card.Img variant="top" src={"https://delivery.royale.uz/storage/" + image}/>
+            <Card className={'col-4 align-items-center text-center align-content-between'}>
+                <div className={'card-img-top'}>
+                    <div className={'card-img-top'}></div>
+                    <Card.Img style={{borderRadius: "5rem"}} variant="top" src={"https://delivery.royale.uz/storage/" + image}/>
+                </div>
                 <Card.Body>
                     <span>{title}</span>
                     <span>{receipt}</span>
