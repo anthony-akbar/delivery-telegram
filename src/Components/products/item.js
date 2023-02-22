be07be232cc43936ac4235fd63a1dce07efb6abb
anthony-akbar <mamadjanovakbar@gmail.com>
2023-02-22 11:38:32 +0500

    card style

--- src/Components/products/item.js
+++ src/Components/products/item.js
@@ -6,12 +6,12 @@ class ProductItem extends Component {
     render() {
         const {title, image, price, status, id} = this.props
         return (
-            <div className={'col-4 align-items-center text-center align-content-between p-5'}>
+            <div className={'col-4 text-center p-1'}>
                 <div className={'card-img-top'}>
                     <div className={'card-img-top'}></div>
-                    <Card.Img style={{borderRadius: "5rem"}} variant="top" src={"https://delivery.royale.uz/storage/" + image}/>
+                    <Card.Img style={{borderRadius: "5%"}} variant="top" src={"https://delivery.royale.uz/storage/" + image}/>
                 </div>
-                <div className={'py-4'} >{title} * {price}</div>
+                <div style={{fontSize:'10px'}} className={'tg-text-color py-4'} >{title} * {price}</div>
                 <div className={'actions'}>
                     <button className={'tg-button btn fw-bold'}>ADD</button>
                 </div>
